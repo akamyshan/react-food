1611e81f793c1c9b826809bcfea670630c90e782
Artem Kamyshan <artem.kamyshan@yandex.ru>
2023-04-02 14:12:57 +0300

    Initial project version

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+function Header() {
+    return (
+        <nav className="green darken-3">
+            <div className="nav-wrapper">
+                <a href="/react-shop" className="brand-logo">
+                    React Shop
+                </a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li>
+                        <a
+                            href="https://github.com/akamyshan/react-shop"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Repo
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/akamyshan">Github</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export { Header };
