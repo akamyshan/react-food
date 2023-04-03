99495c29146cde32791ad4609bc7913d1b64982d
Artem Kamyshan <artem.kamyshan@yandex.ru>
2023-04-03 10:52:20 +0300

    config for api created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ function Header() {
     return (
         <nav className="green darken-3">
             <div className="nav-wrapper">
-                <a href="/react-shop" className="brand-logo">
-                    React Shop
+                <a href="/" className="brand-logo">
+                    React Food
                 </a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li>
